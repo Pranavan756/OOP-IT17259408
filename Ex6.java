bfc2772e98cc49a1bf9771ec26a8d4606c9b8614
pranavan756 <student@202-47.std.sliita.metro>
2020-01-21 17:53:39 +0530

    Area of circle

---
+++ Ex6.java
@@ -0,0 +1,10 @@
+class Area
+{
+    public static void main(String[] args)
+    {
+        int radius=Integer.parseInt(args[0]);
+    
+    double answer=Math.PI*Math.pow(radius,2);
+    System.out.println("Answer"+answer);
+    }
+}
